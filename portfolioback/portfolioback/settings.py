cfc90dde78420844e2467afcc3e6a1c31a8e1ba0
aquarell <aquarell.dev@gmail.com>
2022-09-28 14:42:56 +0300

    added project app to global apps

--- portfolioback/portfolioback/settings.py
+++ portfolioback/portfolioback/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,8 +20,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+
     # apps
     'information.apps.InformationConfig',
+    'project.apps.ProjectConfig'
 ]
 
 MIDDLEWARE = [
@@ -84,5 +88,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
